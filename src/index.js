14cc6268dd19eeed21c7a0fb20f4a74edd6f38ec
Benjamin Hoffman <bhoffman8@gmail.com>
2017-07-15 10:22:42 -0700

    adds webpack support for files, images, etc

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 
 import _ from 'lodash'
+import css from './styles.css'
+import Gh from './gh.png'
 
 function component () {
   const element = document.createElement('div')
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
+  element.classList.add('hello')
+  const image = new Image()
+  image.src = `./dist/${Gh}`
+  element.appendChild(image)
   return element
 }
 
